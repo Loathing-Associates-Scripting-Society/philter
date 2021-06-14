d9b7e458d1b65dca3e45171043ce436a4bced365
pastelmind <keepyourhonor@gmail.com>
2021-06-14 10:27:10 +0900

    perf(manager): Optimize <SelectCleanupAction>
    - `<SelectCleanupAction>` no longer uses `<HTMLSelect>`. Instead, it
      directly creates a `<select>`.
    - Instead of using an `<Icon>`, display the browser's native downwards
      arrow.
    
    Some benchmarks in dev mode (<TableItemCleanup>, initial render):
    
    - Before: ~370 ms
    - After:  ~320 ms

--- packages/manager/src/components/SelectCleanupAction.tsx
+++ packages/manager/src/components/SelectCleanupAction.tsx
@@ -1,5 +1,5 @@
-import {Colors, HTMLSelect} from '@blueprintjs/core';
-import {isCleanupAction, CleanupAction, ItemInfo} from '@philter/common';
+import {Classes, Colors} from '@blueprintjs/core';
+import {CleanupAction, isCleanupAction, ItemInfo} from '@philter/common';
 import classNames from 'classnames';
 import React, {memo} from 'react';
 import {shouldWarnOnPulverize, typeCheck} from '../util';
@@ -26,13 +26,10 @@ interface SelectCleanupActionPropsBase {
 
 interface SelectCleanupActionProps
   extends SelectCleanupActionPropsBase,
-    Omit<
-      React.ComponentProps<typeof HTMLSelect>,
-      keyof SelectCleanupActionPropsBase
-    > {}
+    Omit<React.ComponentProps<'div'>, keyof SelectCleanupActionPropsBase> {}
 
 /**
- * `<HTMLSelect>` element for picking a cleanup action.
+ * `<select>` element for picking a cleanup action.
  */
 // eslint-disable-next-line prefer-arrow-callback
 export const SelectCleanupAction = memo(function SelectCleanupAction({
@@ -43,89 +40,98 @@ export const SelectCleanupAction = memo(function SelectCleanupAction({
   ...restProps
 }: SelectCleanupActionProps) {
   return (
-    <HTMLSelect
-      className={classNames('SelectCleanupAction', className)}
-      onChange={e =>
-        isCleanupActionOrUnknown(e.target.value) && onChange(e.target.value)
-      }
-      value={value}
+    <div
+      className={classNames(
+        Classes.HTML_SELECT,
+        'SelectCleanupAction',
+        className
+      )}
       {...restProps}
     >
-      <option value={typeCheck<CleanupActionOrUnknown>(UNKNOWN_CLEANUP_ACTION)}>
-        (uncategorized)
-      </option>
-      <option value={typeCheck<CleanupActionOrUnknown>('KEEP')}>
-        Keep all
-      </option>
-      {item.canMall && (
-        <option value={typeCheck<CleanupActionOrUnknown>('MALL')}>
-          Mall sale
-        </option>
-      )}
-      {item.canBreak && (
-        <option value={typeCheck<CleanupActionOrUnknown>('BREAK')}>
-          Break apart
-        </option>
-      )}
-      {item.canAutosell && (
-        <option value={typeCheck<CleanupActionOrUnknown>('AUTO')}>
-          Autosell
-        </option>
-      )}
-      {item.canDiscard && (
+      <select
+        onChange={e =>
+          isCleanupActionOrUnknown(e.target.value) && onChange(e.target.value)
+        }
+        value={value}
+      >
         <option
-          style={{color: Colors.ORANGE2}}
-          value={typeCheck<CleanupActionOrUnknown>('DISC')}
+          value={typeCheck<CleanupActionOrUnknown>(UNKNOWN_CLEANUP_ACTION)}
         >
-          Discard
+          (uncategorized)
         </option>
-      )}
-      {item.canGift && (
-        <option value={typeCheck<CleanupActionOrUnknown>('GIFT')}>
-          Send as gift
+        <option value={typeCheck<CleanupActionOrUnknown>('KEEP')}>
+          Keep all
         </option>
-      )}
-      {item.canStash && (
-        <option value={typeCheck<CleanupActionOrUnknown>('CLAN')}>
-          Put in clan stash
+        {item.canMall && (
+          <option value={typeCheck<CleanupActionOrUnknown>('MALL')}>
+            Mall sale
+          </option>
+        )}
+        {item.canBreak && (
+          <option value={typeCheck<CleanupActionOrUnknown>('BREAK')}>
+            Break apart
+          </option>
+        )}
+        {item.canAutosell && (
+          <option value={typeCheck<CleanupActionOrUnknown>('AUTO')}>
+            Autosell
+          </option>
+        )}
+        {item.canDiscard && (
+          <option
+            style={{color: Colors.ORANGE2}}
+            value={typeCheck<CleanupActionOrUnknown>('DISC')}
+          >
+            Discard
+          </option>
+        )}
+        {item.canGift && (
+          <option value={typeCheck<CleanupActionOrUnknown>('GIFT')}>
+            Send as gift
+          </option>
+        )}
+        {item.canStash && (
+          <option value={typeCheck<CleanupActionOrUnknown>('CLAN')}>
+            Put in clan stash
+          </option>
+        )}
+        {item.canPulverize && (
+          <option
+            style={
+              shouldWarnOnPulverize(item) ? {color: Colors.ORANGE2} : undefined
+            }
+            value={typeCheck<CleanupActionOrUnknown>('PULV')}
+          >
+            Pulverize
+          </option>
+        )}
+        {item.canMake && (
+          <option value={typeCheck<CleanupActionOrUnknown>('MAKE')}>
+            Craft...
+          </option>
+        )}
+        {item.canUntinker && (
+          <option value={typeCheck<CleanupActionOrUnknown>('UNTN')}>
+            Untinker
+          </option>
+        )}
+        {item.canUse && (
+          <option value={typeCheck<CleanupActionOrUnknown>('USE')}>Use</option>
+        )}
+        {item.canCloset && (
+          <option value={typeCheck<CleanupActionOrUnknown>('CLST')}>
+            Closet
+          </option>
+        )}
+        {item.canDisplay && (
+          <option value={typeCheck<CleanupActionOrUnknown>('DISP')}>
+            Display
+          </option>
+        )}
+        <option value={typeCheck<CleanupActionOrUnknown>('TODO')}>
+          Reminder
         </option>
-      )}
-      {item.canPulverize && (
-        <option
-          style={
-            shouldWarnOnPulverize(item) ? {color: Colors.ORANGE2} : undefined
-          }
-          value={typeCheck<CleanupActionOrUnknown>('PULV')}
-        >
-          Pulverize
-        </option>
-      )}
-      {item.canMake && (
-        <option value={typeCheck<CleanupActionOrUnknown>('MAKE')}>
-          Craft...
-        </option>
-      )}
-      {item.canUntinker && (
-        <option value={typeCheck<CleanupActionOrUnknown>('UNTN')}>
-          Untinker
-        </option>
-      )}
-      {item.canUse && (
-        <option value={typeCheck<CleanupActionOrUnknown>('USE')}>Use</option>
-      )}
-      {item.canCloset && (
-        <option value={typeCheck<CleanupActionOrUnknown>('CLST')}>
-          Closet
-        </option>
-      )}
-      {item.canDisplay && (
-        <option value={typeCheck<CleanupActionOrUnknown>('DISP')}>
-          Display
-        </option>
-      )}
-      <option value={typeCheck<CleanupActionOrUnknown>('TODO')}>
-        Reminder
-      </option>
-    </HTMLSelect>
+      </select>
+    </div>
   );
 });
