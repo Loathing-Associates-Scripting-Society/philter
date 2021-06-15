c0b8ac56226f0a3d04da96c28522afcc562ae2a3
pastelmind <keepyourhonor@gmail.com>
2021-06-15 16:11:38 +0900

    chore: Update dependencies 2021-06-15

--- release/relay/relay_Philter_Manager.js
+++ release/relay/relay_Philter_Manager.js
@@ -74,10 +74,14 @@ function __read(o, n) {
     return ar;
 }
 
-function __spreadArray(to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
+function __spreadArray(to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || from);
 }
 
 /**
